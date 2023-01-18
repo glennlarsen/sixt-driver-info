7cb16718d13c4b83fdd3da92f6932ca0fddb7264
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-01-18 19:32:58 +0100

    form sent animation

--- src/components/forms/FormsForm/index.jsx
+++ src/components/forms/FormsForm/index.jsx
@@ -8,11 +8,15 @@ const FormsForm = () => {
   const navigate = useNavigate();
 
   const clickForm1 = () => {
-    navigate("/form/sola1");
+    navigate("/form/flesland1");
   };
 
   const clickForm2 = () => {
-    navigate("/form/sola2");
+    navigate("/form/flesland2");
+  };
+
+  const clickForm3 = () => {
+    navigate("/form/flesland3");
   };
 
   return (
@@ -20,19 +24,21 @@ const FormsForm = () => {
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid xs={6}>
           <Paper onClick={clickForm1} className="box">
-            Sola 1
+            Flesland 1
           </Paper>
         </Grid>
         <Grid xs={6}>
           <Paper onClick={clickForm2} className="box">
-            Sola 2
+            Flesland 2
           </Paper>
         </Grid>
         <Grid xs={6}>
-          <Paper className="box box-inactive">Sola 3</Paper>
+          <Paper onClick={clickForm3} className="box">
+            Flesland 3
+          </Paper>
         </Grid>
         <Grid xs={6}>
-          <Paper className="box box-inactive">Sola 4</Paper>
+          <Paper className="box box-inactive">Flesland 4</Paper>
         </Grid>
         <Grid xs={6}>
           <Paper className="box-new box-inactive">New form</Paper>
